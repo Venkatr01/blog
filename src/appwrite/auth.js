49093b2cca4db8f096c88351cb1685a3a479bb3d
Venkatr01 <vraju4865@gmail.com>
2024-03-19 22:43:11 +0530

    add setup

---
+++ src/appwrite/auth.js
@@ -0,0 +1,11 @@
+import conf from '../conf/conf.js'
+import { Client, Account, ID } from "appwrite";
+
+
+export class AuthService{
+    
+}
+
+const authService = new AuthService();
+
+export default authService
