72796abfd5d809a028384fb04810383a40577201
Venkatr01 <vraju4865@gmail.com>
2024-03-21 23:05:55 +0530

    Set up page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import { Header,Footer } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
+  useEffect(() =>{
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+
+  },[])
+
+  return !loading ? (
     <>
-      <h3>A Blog App Using Appwrite</h3>
+      <div className='min-h-screen flex flex-wrap
+      content-between bg-gray-500'>
+        <div className='w-full block'>
+          <Header />
+          <main>
+            {/* <Outlet /> */}
+          </main>
+          <Footer />
+        </div>
+      </div>
     </>
-  )
+  ) : null
 }
 
 export default App
